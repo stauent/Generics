74526d42f1030ea2f1aa4c04ee465f775728428b
stauent <stauent@yahoo.ca>
2020-11-28 12:23:44 -0500

    Updated code

--- Generics/DoubleLinkedList.cs
+++ Generics/DoubleLinkedList.cs
@@ -268,5 +268,6 @@ namespace Generics
         }
 
 
+
     }
 }
--- Generics/InitializableObjects.cs
+++ Generics/InitializableObjects.cs
@@ -6,7 +6,7 @@ namespace Generics
 {
     public static class ObjectFactory 
     {
-        public static T Create<T>(string Name) where T : class, ICommunicate, new()
+        public static T Create<T>(string Name) where T :  ICommunicate, new()
         {
             T newObject = new T();
             newObject.Initialize(Name);
